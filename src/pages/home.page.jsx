519c2ccd5d2e1d9ed7d38ac4fa7e6a378ff806e5
Idsarth <idsarthdev19@gmail.com>
2020-06-26 21:53:17 -0600

    :speech_balloon: Idsarth - develop
    Agregando informacion en la pagina de about

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { GrMapLocation } from 'react-icons/gr';
 
 import Title from '../components/title.component';
 import Layout from '../layout/layout';
@@ -6,6 +7,17 @@ import Layout from '../layout/layout';
 const HomePage = () => (
   <Layout>
     <Title title="About Me" />
+    <h2>Idsarth Juarez</h2>
+    <div>
+      <GrMapLocation />
+      <span>Managua, Nicaragua</span>
+    </div>
+    <p>
+      Hello! I'm Idsarth Juarez FullStack and Mobile developer,
+      with extensive experience in developing native and multi-platform applications for
+      iOS and Android. With solid knowledge in technologies such as
+      ReactJS, React Native, NextJS, NodeJS, Graphql, Flutter, Golang and Python.
+    </p>
   </Layout>
 );
 
