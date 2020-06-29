f9fb49673e34a3aaa40a6fcc7f2f0b83f88a1854
Idsarth <idsarthdev19@gmail.com>
2020-06-28 20:02:11 -0600

    Updates

--- precache-manifest.62a7ef5b52f86fc82050ed84f2a3e602.js
+++ precache-manifest.62a7ef5b52f86fc82050ed84f2a3e602.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4b285ed63584e7ef475d38cd478d3d71",
+    "revision": "4f3279668796e20dbf957029434dd659",
     "url": "/index.html"
   },
   {
-    "revision": "35c60c2404675b6ab8e2",
+    "revision": "12e64604e714195fba12",
     "url": "/static/css/main.a5ce1b3d.chunk.css"
   },
   {
-    "revision": "9c44a144ca4cc107a72a",
-    "url": "/static/js/2.af399fb2.chunk.js"
+    "revision": "0103956bff6b5b18df59",
+    "url": "/static/js/2.fa044c6f.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/static/js/2.af399fb2.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.fa044c6f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "35c60c2404675b6ab8e2",
-    "url": "/static/js/main.11ab1dae.chunk.js"
+    "revision": "12e64604e714195fba12",
+    "url": "/static/js/main.14e31872.chunk.js"
   },
   {
     "revision": "3a567a759692b3447f99",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1b84d85c1518134a794ef60b412d48ac.js"
+  "/precache-manifest.62a7ef5b52f86fc82050ed84f2a3e602.js"
 );
 
 self.addEventListener('message', (event) => {
