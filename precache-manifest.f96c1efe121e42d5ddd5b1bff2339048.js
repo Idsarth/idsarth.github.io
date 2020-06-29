4364c299fc5766a750f39253f99540ea1a23b12d
Idsarth <idsarthdev19@gmail.com>
2020-06-28 18:38:40 -0600

    Updates

---
+++ precache-manifest.f96c1efe121e42d5ddd5b1bff2339048.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ffe198bb3da8f9223f5137f770ebedb",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "35c60c2404675b6ab8e2",
+    "url": "/portfolio/static/css/main.a5ce1b3d.chunk.css"
+  },
+  {
+    "revision": "9c44a144ca4cc107a72a",
+    "url": "/portfolio/static/js/2.af399fb2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.af399fb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35c60c2404675b6ab8e2",
+    "url": "/portfolio/static/js/main.11ab1dae.chunk.js"
+  },
+  {
+    "revision": "ec9dbe8eb76dbe993038",
+    "url": "/portfolio/static/js/runtime-main.e1089ece.js"
+  }
+]);
